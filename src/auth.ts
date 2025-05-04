4133505394b26a1c6ed91fbe61f6dd785b6fc49c
Carlos Costa <carllos.nc@gmail.com>
2025-05-04 05:46:51 -0300

    Refactor auth.ts to update user-schema import path and change session strategy to database

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db, accounts, users, sessions, verificationTokens } from "@/data/user-schema"
+import { db, accounts, users, sessions, verificationTokens } from "@/data/db-schemas/user-schema"
 import Google from "next-auth/providers/google"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
@@ -11,7 +11,15 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     verificationTokensTable: verificationTokens,
   }),
   providers: [Google],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id
+      return session
+    },
+  },
   session: {
-    strategy: "jwt",
+    strategy: "database",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
   },
 })
