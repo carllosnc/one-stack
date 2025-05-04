02485fb192028049b105badba1c99865d7e92fef
Carlos Costa <carllos.nc@gmail.com>
2025-05-04 05:47:31 -0300

    Refactor Dashboard component to retrieve userId from session and improve layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,15 @@
+import { NewTodo, ListTodos } from "@/components/dashboard/new-todo"
 import { checkSession } from "@/lib/check-session"
 
 export default async function Dashboard() {
-  await checkSession()
+  const session = await checkSession()
+  const userId = session?.user?.id as string
 
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center">
-      <h1>Dashboard</h1>
+    <div className="w-full min-h-screen gap-[20px] flex flex-col justify-center items-center">
+      <NewTodo userId={userId} />
+      <hr className="border-black w-full" />
+      <ListTodos userId={userId} />
     </div>
   )
 }
