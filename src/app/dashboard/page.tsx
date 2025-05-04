6cfa5d7bc64f4aa5b57ed47fa39a2dd1866bb171
Carlos Costa <carllos.nc@gmail.com>
2025-05-04 14:55:14 -0300

    Remove NewTodo and ListTodos components from Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,14 @@
-import { NewTodo, ListTodos } from "@/components/dashboard/new-todo"
 import { checkSession } from "@/lib/check-session"
 
 export default async function Dashboard() {
   const session = await checkSession()
   const userId = session?.user?.id as string
 
+  console.log(userId)
+
   return (
     <div className="w-full min-h-screen gap-[20px] flex flex-col justify-center items-center">
-      <NewTodo userId={userId} />
-      <hr className="border-black w-full" />
-      <ListTodos userId={userId} />
+      <h1> Dashboard </h1>
     </div>
   )
 }
